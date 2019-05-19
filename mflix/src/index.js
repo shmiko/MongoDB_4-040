53bec540da85e24682633aaccc9ec1bd4d35b281
Paul Jones <shmiko@gmail.com>
2019-05-19 13:48:07 +1000

    conection pooling

--- mflix/src/index.js
+++ mflix/src/index.js
@@ -22,14 +22,14 @@ concern timeout limit to 2500 milliseconds.
 
 MongoClient.connect(
   process.env.MFLIX_DB_URI, {
-    poolSize: 50, ssl: true,
-    connectTimeoutMS : 2500
+    poolSize: 50, ssl: true
   },
   // TODO: Connection Pooling
   // Set the poolSize to 50 connections.
   // TODO: Timeouts
   // Set the write timeout limit to 2500 milliseconds.
   { useNewUrlParser: true },
+  {connectTimeoutMS : 2500}
 )
   .catch(err => {
     console.error(err.stack)
