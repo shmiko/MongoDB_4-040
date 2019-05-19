56162f28cd71a42349ed6013fec07c2c75ec578b
Paul Jones <shmiko@gmail.com>
2019-05-19 14:05:31 +1000

    error handling

--- mflix/test/config/mongoEnvironment.js
+++ mflix/test/config/mongoEnvironment.js
@@ -4,15 +4,15 @@ module.exports = class MongoEnvironment extends NodeEnvironment {
   async setup() {
     if (!this.global.mflixClient) {
       this.global.mflixClient = await MongoClient.connect(
-        process.env.MFLIX_DB_URI, {
-          poolSize: 50, ssl: true
-        },
         // TODO: Connection Pooling
         // Set the poolSize to 50 connections.
         // TODO: Timeouts
         // Set the write timeout limit to 2500 milliseconds.
-        { useNewUrlParser: true ,
-        wtimeout : 2500}
+        process.env.MFLIX_DB_URI, {
+          poolSize: 50, ssl: true,
+          useNewUrlParser: true ,
+          wtimeout : 2500,
+        }
       )
       await super.setup()
     }
