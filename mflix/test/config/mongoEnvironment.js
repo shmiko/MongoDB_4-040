59c598255fdf76be395e195a6f87062a05ab69e5
Paul Jones <shmiko@gmail.com>
2019-05-27 15:31:00 +1000

    chaner server port

--- mflix/test/config/mongoEnvironment.js
+++ mflix/test/config/mongoEnvironment.js
@@ -27,3 +27,20 @@ module.exports = class MongoEnvironment extends NodeEnvironment {
     return super.runScript(script)
   }
 }
+
+
+const baseballPlayers2 = [
+  { insertOne: { '_id': 11, 'name': 'Edgar Martinez', 'salary': "8.5M" }},    // Insert #1
+  { insertOne: { '_id': 3, 'name': 'Alex Rodriguez', 'salary': "18.3M" }},    // Insert #2
+  { insertOne: { '_id': 24, 'name': 'Ken Griffey Jr.', 'salary': "12.4M" }},  // Insert #3
+  { insertOne: { '_id': 11, 'name': 'David Bell', 'salary': "2.5M" }},        // Insert #4
+  { insertOne: { '_id': 19, 'name': 'Jay Buhner', 'salary': "5.1M" }}         // Insert #5
+]
+
+const bulkWriteResponse = db.employees.bulkWrite([
+  { insertOne: { '_id': 11, 'name': 'Edgar Martinez', 'salary': "8.5M" }},    
+  { insertOne: { '_id': 3, 'name': 'Alex Rodriguez', 'salary': "18.3M" }},    
+  { insertOne: { '_id': 24, 'name': 'Ken Griffey Jr.', 'salary': "12.4M" }},  
+  { insertOne: { '_id': 11, 'name': 'David Bell', 'salary': "2.5M" }},        
+  { insertOne: { '_id': 19, 'name': 'Jay Buhner', 'salary': "5.1M" }}         
+])
